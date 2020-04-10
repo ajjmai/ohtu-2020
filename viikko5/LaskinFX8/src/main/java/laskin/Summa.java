95c87366d7064a6da0ffb34cc0202b3b7e34fd9e
Aija <aija.valleala@gmail.com>
2020-04-10 08:44:54 +0300

    tehtävä 4

---
+++ viikko5/LaskinFX8/src/main/java/laskin/Summa.java
@@ -0,0 +1,21 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Summa extends Komento {
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        sovellus.plus(getArvo());
+        setLaskunTulos();
+    }
+
+    @Override
+    public void peru() {
+    }
+
+}
