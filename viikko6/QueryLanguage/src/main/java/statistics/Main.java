721efa2364c1e65f5730672e669d02d16032dbbb
Aija <aija.valleala@gmail.com>
2020-04-13 10:23:20 +0300

    tehtävä 4

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -1,7 +1,5 @@
 package statistics;
 
-import javax.sound.sampled.SourceDataLine;
-
 import statistics.matcher.*;
 
 public class Main {
@@ -13,7 +11,8 @@ public class Main {
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
 
-        Matcher m = new And(new HasAtLeast(5, "goals"), new HasAtLeast(5, "assists"), new PlaysIn("PHI"));
+        QueryBuilder query = new QueryBuilder();
+        Matcher m = query.playsIn("NYR").build();
 
         for (Player player : stats.matches(m)) {
             System.out.println(player);
@@ -21,36 +20,53 @@ public class Main {
 
         System.out.println("");
 
-        Matcher m2 = new And(new HasFewerThan(1, "goals"), new PlaysIn("NYR"));
+        Matcher m2 = query.playsIn("NYR").hasAtLeast(5, "goals").hasFewerThan(10, "goals").build();
 
         for (Player player : stats.matches(m2)) {
             System.out.println(player);
         }
 
-        System.out.println("");
+        // Matcher m = new And(new HasAtLeast(5, "goals"), new HasAtLeast(5, "assists"),
+        // new PlaysIn("PHI"));
 
-        Matcher m3 = new And(new Not(new HasAtLeast(1, "goals")), new PlaysIn("NYR"));
+        // for (Player player : stats.matches(m)) {
+        // System.out.println(player);
+        // }
 
-        for (Player player : stats.matches(m3)) {
-            System.out.println(player);
-        }
+        // System.out.println("");
 
-        System.out.println("");
+        // Matcher m2 = new And(new HasFewerThan(1, "goals"), new PlaysIn("NYR"));
 
-        Matcher m4 = new Or(new HasAtLeast(20, "goals"), new HasAtLeast(20, "assists"));
+        // for (Player player : stats.matches(m2)) {
+        // System.out.println(player);
+        // }
 
-        for (Player player : stats.matches(m4)) {
-            System.out.println(player);
-        }
+        // System.out.println("");
 
-        System.out.println("");
+        // Matcher m3 = new And(new Not(new HasAtLeast(1, "goals")), new
+        // PlaysIn("NYR"));
 
-        Matcher m5 = new And(new HasAtLeast(20, "points"),
-                new Or(new PlaysIn("NYR"), new PlaysIn("NYI"), new PlaysIn("NJD")));
+        // for (Player player : stats.matches(m3)) {
+        // System.out.println(player);
+        // }
 
-        for (Player player : stats.matches(m5)) {
-            System.out.println(player);
-        }
+        // System.out.println("");
+
+        // Matcher m4 = new Or(new HasAtLeast(20, "goals"), new HasAtLeast(20,
+        // "assists"));
+
+        // for (Player player : stats.matches(m4)) {
+        // System.out.println(player);
+        // }
+
+        // System.out.println("");
+
+        // Matcher m5 = new And(new HasAtLeast(20, "points"),
+        // new Or(new PlaysIn("NYR"), new PlaysIn("NYI"), new PlaysIn("NJD")));
+
+        // for (Player player : stats.matches(m5)) {
+        // System.out.println(player);
+        // }
 
     }
 }
